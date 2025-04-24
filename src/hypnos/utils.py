fa5d964fd7c8d52a3f0f573a71b5fca4feb58735
Tuan Vu <vuanhtuan1407@gmail.com>
2025-04-25 03:02:06 +0700

    update conf

--- src/hypnos/utils.py
+++ src/hypnos/utils.py
@@ -3,11 +3,11 @@ import argparse
 
 def training_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default="./config/config.example.yml")
+    parser.add_argument("--config", type=str, default="./config/conf.yml.example")
     return parser.parse_args()
 
 
 def data_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_config", type=str, default="./config/data_config.example.yml")
+    parser.add_argument("--data_config", type=str, default="./config/data_conf.yml.example")
     return parser.parse_args()
